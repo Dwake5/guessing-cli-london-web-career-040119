d3529e111a22ac9042dda4bbfc81ef1f3623475d
Danny Wakeling <wakeling101@hotmail.com>
2019-03-22 18:35:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+def run_guessing_game
+  rand_number = rand(6)
+  puts "Pick a number between 1 and 6"
+  input = gets
+  while input != 'bye'
+    if input.to_i == rand_number
+      puts "You guessed the correct number!"
+    end
+  end
+end
+
+
+
+
