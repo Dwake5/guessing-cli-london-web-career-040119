fe92d33ac141deff3851703d04cb5e621575f187
Danny Wakeling <wakeling101@hotmail.com>
2019-03-22 18:45:01 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,11 +2,10 @@
 def run_guessing_game
   loop do
     rand_number = rand(6)
-    puts "Guess a number between 1 and 6"
+    puts "Guess a number between 1 and 6."
     input = gets
     if input == "exit"
-      puts "Goodbye!"
-    break
+      break
     elsif input.to_i == rand_number
       puts "You guessed the correct number!"
     elsif input.to_i != rand_number
